3cf14d9e56aa790780b26e698f3dd201f89194e8
Josh Wheeler <joshdub223@gmail.com>
2020-04-15 15:11:13 -0500

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ module.exports = {
   ],
   // Customize your site metadata:
   siteMetadata: {
-    title: `My Blog Title`,
-    author: `My Name`,
-    description: `My site description...`,
+    title: `Josh Wheeler Blog`,
+    author: `Josh Wheeler`,
+    description: `A description YEAH!`,
     social: [
       {
         name: `twitter`,
